c535a52b3ce14a6357121029c23a4d2144530955
Mariiya <mariakozusan@gmail.com>
2022-02-14 11:45:38 +0200

    Build fix

--- DomainDictionaryWeb/server.js
+++ DomainDictionaryWeb/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/angular-social'));
 app.get('/*', function(req,res) {
 
-    res.sendFile('index.html',{root:'./dist'});
+    res.sendFile('index.html',{root:'./dist/angular-social'});
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
