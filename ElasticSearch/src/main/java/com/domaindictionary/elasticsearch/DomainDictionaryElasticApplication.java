9624f6072ec16788ae68606d5025aa284b6fdf4e
Mariiya <mariakozusan@gmail.com>
2022-02-14 17:17:23 +0200

    Elastic deploy

--- ElasticSearch/src/main/java/com/domaindictionary/elasticsearch/DomainDictionaryElasticApplication.java
+++ ElasticSearch/src/main/java/com/domaindictionary/elasticsearch/DomainDictionaryElasticApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+
 @PropertySource("classpath:application.properties")
 @SpringBootApplication
 public class DomainDictionaryElasticApplication {
